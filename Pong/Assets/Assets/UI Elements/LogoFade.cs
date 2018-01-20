0645e0494ce7cf19c82b19178c5d8fb7c5e3d9da
Chance Lytle <chance_lytle@yahoo.com>
2018-01-20 00:23:33 -0500

    Basic UI
    Splash Scenes + Main Menu UI

---
+++ Pong/Assets/Assets/UI Elements/LogoFade.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;using UnityEngine.SceneManagement;
+
+public class LogoFade : MonoBehaviour
+{
+
+    public Image firstLogo;
+    public Image secondLogo;
+    // Next level to load will most likely be main menu.  Explore possibilities of making this modular and having multiple Splash Scenes for better OOP instead of all in one
+
+    IEnumerator Start()
+    {
+        firstLogo.canvasRenderer.SetAlpha(0.0f);
+        secondLogo.canvasRenderer.SetAlpha(0.0f);
+        // Fade in/out first logo
+        FadeIn(firstLogo);
+        yield return new WaitForSeconds(2.0f);
+        FadeOut(firstLogo);
+        yield return new WaitForSeconds(1.0f);
+        // Fade in/out second logo
+        FadeIn(secondLogo);
+        yield return new WaitForSeconds(2.0f);
+        FadeOut(secondLogo);
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // transition to next scene
+    }
+
+    void FadeIn(Image img)
+    {
+        img.CrossFadeAlpha(1.0f, 1.0f, false);
+    }
+
+    void FadeOut(Image img)
+    {
+        img.CrossFadeAlpha(0.0f, 1.0f, false);
+    }
+}
---
+++ Pong/Assets/Assets/UI Elements/MainMenuControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControl : MonoBehaviour {
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        // Add desired logic for quit here
+        Application.Quit();
+    }
+}
