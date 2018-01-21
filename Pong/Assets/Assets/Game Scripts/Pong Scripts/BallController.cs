b7da8b9efa6e9d77c1867eeee6a88c87631da075
alicefaere <alicefaere@users.noreply.github.com>
2018-01-21 18:16:02 -0500

    Basic Physical Movement
    The ball has simple Pong bouncing physics.
    Made the top and side walls fade away after they're hit to keep up the ping pong look.
    Made the table static
    Added "net" to table (non physical)

---
+++ Pong/Assets/Assets/Game Scripts/Pong Scripts/BallController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 direction;
+
+	void Start ()
+    {
+	    rb = GetComponent<Rigidbody>();
+        direction = new Vector3(0.5f, 0.75f, 0f).normalized;
+    }
+
+	void FixedUpdate ()
+    {
+        //rb.AddForce(direction);
+        rb.velocity = direction;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Hwall"))
+        {
+            direction = new Vector3(direction.x, -direction.y, direction.z);
+        }
+        else if (other.gameObject.CompareTag("Vwall"))
+        {
+            direction = new Vector3(-direction.x, direction.y, direction.z);
+        }
+        else if (other.gameObject.CompareTag("Zwall"))
+        {
+            //this should probably do something unique
+            direction = new Vector3(direction.x, direction.y, -direction.z);
+        }
+    }
+}
---
+++ Pong/Assets/Assets/Game Scripts/Pong Scripts/WallPopIn.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class WallPopIn : MonoBehaviour
+{
+    private const float timeToShow = 1.0f;
+    private float timer;
+    private MeshRenderer mr;
+    private Renderer m;
+
+	void Start ()
+	{
+	    mr = GetComponent<MeshRenderer>();
+	    m = GetComponent<Renderer>();
+	    mr.enabled = false;
+	}
+	
+	void Update ()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            m.material.color = new Color(m.material.color.r, m.material.color.g, m.material.color.b, timer);
+            if (timer < 0)
+            {
+                mr.enabled = false;
+            }
+
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            mr.enabled = true;
+            timer = timeToShow;
+            m.material.color = new Color(m.material.color.r, m.material.color.g, m.material.color.b, timer);
+        }
+    }
+}
