8a99b6a7542440ba5b153ec89cc26a3e6a42e786
Aishwarya Srivastava <Srivastava.109@osu.edu>
2018-01-21 03:15:19 -0500

    Added layout for main game
    - Background with moving clouds (can add more characters later)
    - Collision handling between table + ball and table + ground
    Note: I still have to get rid of unused assets so pull with caution

---
+++ Pong/Assets/Assets/Game Scripts/CloudManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudManager : MonoBehaviour {
+
+    private Vector3 speed;
+    private int cloudCount;
+	// Use this for initialization
+	void Start () {
+        cloudCount = gameObject.transform.childCount;
+        speed = new Vector3(0.05f, 0.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < cloudCount; i++){
+            GameObject cloud = transform.GetChild(i).gameObject;
+            Vector3 pos = cloud.transform.position;
+            pos = pos + speed;
+            if (pos.x > 150)
+            {
+                pos.x = -40;
+            }
+            cloud.transform.position = pos;
+        }
+
+	}
+}
