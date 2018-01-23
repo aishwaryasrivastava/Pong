341209c46aeeb4fd358cff42654366b81945b881
Aishwarya Srivastava <Srivastava.109@osu.edu>
2018-01-23 00:25:03 -0500

    Added dark mode
    Dark mode!
    - Toggle UI
    - Smooth dimming/lighting of the environment upon toggling
    -ball glows overtime it collides wall (different colors for paddles)

--- Pong/Assets/Assets/Game Scripts/Pong Scripts/BallController.cs
+++ Pong/Assets/Assets/Game Scripts/Pong Scripts/BallController.cs
@@ -3,17 +3,32 @@
 public class BallController : MonoBehaviour
 {
     public float zMin, zMax, Speed, BounceMult;
-
+    private bool darkMode;
     private const float zMove = 1;
     private float localSpeed;
     private Rigidbody rb;
     private Vector3 direction;
-    private int nextDir = 1;   
+    private int nextDir = 1;
+    private Light halo;
+    // State of the ball
+    private bool glowingUp, glowingDown;
+    // State of the environment
+    private bool environmentDimming, environmentLightingUp;
+    public Light MainLight;
+
+    public void toggleDarkMode(bool newValue){
+        darkMode = newValue;
+        environmentDimming = darkMode;
+        environmentLightingUp = !darkMode;
+    }
 
 	void Start ()
     {
 	    rb = GetComponent<Rigidbody>();
         ResetBall();
+        halo = gameObject.GetComponent<Light>();
+
+
     }
 
     void ResetBall()
@@ -22,22 +37,72 @@ public class BallController : MonoBehaviour
         direction = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), nextDir *2).normalized; //test velocity
         transform.localPosition = new Vector3(0, 10, 10 - nextDir*10); //center
         localSpeed = Speed;
+        glowingUp = false;
+        glowingDown = false;
+        environmentDimming = false;
+        environmentLightingUp = false;
     }
 
     void Update()
     {
+        // Smooth Dimming/Lighting up when dark mode is toggled
+        float intensity = MainLight.intensity;
+        if (environmentDimming) {
+            if (intensity > 0)
+            {
+                intensity = intensity - 0.1f;
+            }
+            else {
+                environmentDimming = false;
+            }
+        }
+        if (environmentLightingUp) {
+            if (intensity < 1){
+                intensity = intensity + 0.1f;
+            }
+            else {
+                environmentLightingUp = false;
+            }
+        }
+        MainLight.intensity = intensity;
+
+        // Moving the ball
         var tmp = transform.localPosition;
         if (tmp.z < zMin)
         {
             //player 2 scored
             nextDir = 1;
             ResetBall();
-        }          
+        }
         else if (tmp.z > zMax)
         {
             //player 1 scored
             nextDir = -1;
             ResetBall();
+        }
+
+        // Glowing the ball in dark mode
+        if (darkMode)
+        {
+            float haloRange = halo.range;
+            if (glowingUp)
+            {
+                if (haloRange < 0.5)
+                {
+                    haloRange = haloRange + 0.05f;
+                }
+                else { glowingUp = false; glowingDown = true; }
+            }
+
+            if (glowingDown)
+            {
+                if (haloRange > 0)
+                {
+                    haloRange = haloRange - 0.05f;
+                }
+                else { glowingUp = false; glowingDown = false; }
+            }
+            halo.range = haloRange;
         }
     }
 
@@ -63,6 +128,26 @@ public class BallController : MonoBehaviour
             var norm = new Vector2(diff.x, diff.y).normalized;
             direction = new Vector3(norm.x, norm.y, -direction.z);
             localSpeed *= BounceMult;
+
+            // Glowing the ball red/blue when it hits a paddle
+            if (darkMode)
+            {
+                glowingUp = true; glowingDown = false;
+                if (other.gameObject.CompareTag("Zwall"))
+                {
+                    halo.color = Color.red;
+
+                }
+                else { halo.color = Color.blue; }
+            }
+        }
+
+        // Glowing the ball white when it hits the other walls/table top
+        if (darkMode&&(other.gameObject.CompareTag("Hwall")||other.gameObject.CompareTag("Vwall"))){
+            glowingUp = true; glowingDown = false;
+            halo.color = Color.white;
         }
     }
+
+
 }
