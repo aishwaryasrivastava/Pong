0f4e084c6be4b70059579ba8787a2ea6fe682675
yuchen97ye <yuchen97ye@gmail.com>
2018-01-24 00:18:50 -0500

    Implemented a power up that can speed up the player paddle

--- Pong/Assets/Assets/Game Scripts/Pong Scripts/Player1Paddle.cs
+++ Pong/Assets/Assets/Game Scripts/Pong Scripts/Player1Paddle.cs
@@ -7,7 +7,10 @@ public class Player1Paddle : MonoBehaviour
     public float xMin, xMax, yMin, yMax, PaddleSpeed;
     private const float size = 5.0f;
     private Rigidbody rb;
-
+    private bool poweredUp;
+    private float powerUpCounter;
+    private float powerUpDuration = 4;
+    private GameObject powerUp;
 	void Start ()
 	{
 	    rb = GetComponent<Rigidbody>();
@@ -28,14 +31,25 @@ public class Player1Paddle : MonoBehaviour
             transform.localPosition = new Vector3(tmp.x, yMin + size, tmp.z);
         else if (tmp.y + size > yMax)
             transform.localPosition = new Vector3(tmp.x, yMax - size, tmp.z);
+        powerUp = GameObject.FindGameObjectWithTag("powerUp");
+        if (powerUp != null)
+        {
+            if ((powerUp.transform.position - transform.position).magnitude < 0.2 && !poweredUp)
+            {
+                poweredUp = true;
+                powerUpCounter = powerUpDuration;
+            }
+        }
+        powerUpCounter -= Time.deltaTime;
+        if (powerUpCounter < 0) { poweredUp = false; }
     }
 
 	void FixedUpdate ()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
-        rb.velocity = PaddleSpeed * new Vector3(moveHorizontal, moveVertical, 0);
+        if (poweredUp) { rb.velocity = 3 * PaddleSpeed * new Vector3(moveHorizontal, moveVertical, 0); }
+        else { rb.velocity = PaddleSpeed * new Vector3(moveHorizontal, moveVertical, 0); }
 
     }
 }
---
+++ Pong/Assets/Assets/Game Scripts/Pong Scripts/PowerUpManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpManager : MonoBehaviour {
+    public GameObject powerUp;
+    private GameObject tmp;
+    private float freq = 5;
+    private float timeCount;
+    private float spd = 2;
+    private float lifeTime;
+    private float maxLife = 1;
+	// Use this for initialization
+	void Start () {
+        tmp = null;
+        timeCount = freq;
+        lifeTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeCount -= Time.deltaTime;
+        if (timeCount < 0)
+        {
+            tmp = Instantiate(powerUp, GameObject.FindGameObjectWithTag("table").transform);
+            timeCount = freq;
+            lifeTime = maxLife;
+        }
+        lifeTime -= Time.deltaTime;
+        if (tmp != null) {
+            tmp.transform.position -= new Vector3(0, 0, spd * Time.deltaTime);
+            if (lifeTime < 0) { Destroy(tmp); }
+        }
+	}
+}
