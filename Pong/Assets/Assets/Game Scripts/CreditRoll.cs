db6e129b07d34926c05de862653781ae266769a2
Spidcock <steven.pidcock@gmail.com>
2018-01-24 23:47:58 -0500

    Added Credits Scene

---
+++ Pong/Assets/Assets/Game Scripts/CreditRoll.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CreditRoll : MonoBehaviour
+{
+
+    public float speed;
+    public float duration;
+
+    private float time;
+
+    // Use this for initialization
+    void Start()
+    {
+        time = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos += new Vector3(0, speed, 0);
+        transform.position = pos;
+
+        time += Time.deltaTime;
+        if (time>duration)
+        {
+            SceneManager.LoadScene("Scenes/MainMenu");
+        }
+    }
+}
