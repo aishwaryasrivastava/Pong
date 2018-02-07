7fcd21244852dc53441dd97014f6407f6a809d5e
yuchen97ye <yuchen97ye@gmail.com>
2018-02-07 14:26:07 -0500

    RageCommitByBob

---
+++ Pong/Assets/Assets/Game Scripts/Shoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    private float timer;
+    private int damage = 30;
+    private float fireGap = 0.1f;
+    private RaycastHit Shot;
+    // Use this for initialization
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            if (timer <= 0)
+            {
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Shot))
+                {
+                    Shot.transform.SendMessage("shotAt", damage, SendMessageOptions.DontRequireReceiver);
+                }
+                GetComponent<Animation>().Play("RecoilAnimation");
+                timer = fireGap;
+            }
+        }
+        timer -= Time.deltaTime;
+    }
+}
---
+++ Pong/Assets/Assets/Game Scripts/ShotAt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotAt : MonoBehaviour
+{
+    private int health;
+    // Use this for initialization
+    void Start()
+    {
+        health = 100;
+    }
+
+    void shotAt(int damage)
+    {
+        health -= damage;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (health < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
