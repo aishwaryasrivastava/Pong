da68ca79ca2eeb59f6373883c3e25b82629df29e
Zicong Zhang <zhangzicong.passion@gmail.com>
2018-02-12 17:51:49 -0500

    Completing guard movement and sound
    Adding walk movements to AI, and 2 extra sound clips found on internet.

--- Pong/Assets/Assets/Game Scripts/Gameplay Scripts/AI/GuardScript.cs
+++ Pong/Assets/Assets/Game Scripts/Gameplay Scripts/AI/GuardScript.cs
@@ -6,11 +6,10 @@ public class GuardScript : MonoBehaviour {
 	public float attackDistance = 5.0f;
 	public float runningDistance = 20.0f;
 	public float speed = 0.2f;
+	public float walkingSpeed = 0.01f;
 
 	private Vector3 position;
 	private Animator anim;
-	private int time = 0;
-	private int limit = 1000;
 	private Vector3 direction;
 
 	void Start () {
@@ -19,26 +18,12 @@ public class GuardScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log (Vector3.Distance (player.position, transform.position));
-		time = time + 1;
-		/*
-		if (time > limit) {
-			anim.SetBool ("isWalking", false);
-			anim.SetBool ("isIdle", true);
-		} else {
-			position = transform.position;
-			position = position + new Vector3(0, 0, 0.05f);
-			transform.position = position;
-			anim.SetBool ("isWalking", true);
-			anim.SetBool ("isIdle", false);
-		}
-		*/
-
 		if ((Vector3.Distance (player.position, transform.position) < runningDistance) && (Vector3.Distance (player.position, transform.position) > attackDistance)) {
 			anim.SetBool ("isRunning", true);
 			anim.SetBool ("isAttacking", false);
 			anim.SetBool ("isIdle", false);
 			anim.SetBool ("switching", false);
+			anim.SetBool ("isWalking", false);
 			direction = player.position - transform.position;
 			direction.y = 0;
 			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation(direction), 0.9f);
@@ -49,14 +34,20 @@ public class GuardScript : MonoBehaviour {
 			anim.SetBool ("switching", true);
 			anim.SetBool ("isIdle", false);
 			anim.SetBool ("switching", false);
-		} else if (anim.GetBool ("isIdle") == false) {
 			anim.SetBool ("isWalking", false);
+		}else if (Vector3.Distance (player.position, transform.position) > runningDistance) {
 			anim.SetBool ("isRunning", false);
 			anim.SetBool ("isAttacking", false);
-			anim.SetBool ("isIdle", true);
+			anim.SetBool ("isIdle", false);
 			anim.SetBool ("switching", false);
-		} else if (Vector3.Distance (player.position, transform.position) > runningDistance) {
-			
+			anim.SetBool ("isWalking", true);
+			transform.Translate (0, 0, walkingSpeed);
 		}
+	}
+
+	private void OnCollisionEnter(Collision other){
+		direction = transform.forward;
+		direction *= -1;
+		transform.rotation = Quaternion.LookRotation (direction);
 	}
 }
