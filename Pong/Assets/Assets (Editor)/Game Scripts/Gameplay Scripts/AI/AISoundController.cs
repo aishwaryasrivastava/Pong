557db54f61e1f98abaa6d333b525d2bd582d2b93
Zicong Zhang <zhangzicong.passion@gmail.com>
2018-02-19 16:35:25 -0500

    Add some sounds to the game.
    Will add more.

---
+++ Pong/Assets/Assets (Editor)/Game Scripts/Gameplay Scripts/AI/AISoundController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISoundController : MonoBehaviour {
+
+	public AudioClip hitSound;
+	public AudioSource source;
+	// Use this for initialization
+	private float lowVol = 0.2f;
+	void Start () {
+		source = GetComponent<AudioSource>();
+	}
+	
+	public void PlayBeat()
+	{
+		source.PlayOneShot(hitSound, lowVol);
+	}
+}
