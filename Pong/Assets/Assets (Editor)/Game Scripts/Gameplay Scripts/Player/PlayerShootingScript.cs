9be55491a7381dce5b2f6856cca989b0ac1da5bc
yuchen97ye <yuchen97ye@gmail.com>
2018-02-19 00:25:58 -0500

    Added guns and targets
    Imported a new asset called Weapon Pack (LOW POLY). Attached an AK-47 to the main camera. Added two scripts, PlayerShootingScript, which is attached to the main camera, and ShotAtScript, which is attached to three targets I added to the prison scene. I also changed the main camera a little bit.

---
+++ Pong/Assets/Assets (Editor)/Game Scripts/Gameplay Scripts/Player/PlayerShootingScript.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class PlayerShootingScript : MonoBehaviour
+{
+    private float timer;
+    private int damage = 30;
+    private float fireGap = 0.1f;
+    private float yaw = -0.044f;
+    private float pitch = 0.044f;
+    private float sensitivity = 0.57f;
+    private RaycastHit Shot;
+    private GameObject flare;
+    private Transform gun;
+    private float recoil;
+    // Use this for initialization
+    void Start()
+    {
+        timer = 0;
+        gun = gameObject.transform.Find("AK-47");
+        flare = gun.Find("Flare").gameObject;
+        flare.SetActive(false);
+        recoil = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Fire();
+    }
+
+    void Fire()
+    {
+        if (timer <= 0)
+        {
+            flare.SetActive(false);
+            if (Input.GetButton("Fire1"))
+            {
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Shot))
+                {
+                    flare.SetActive(true);
+                    Shot.transform.SendMessage("shotAt", damage * transform.TransformDirection(Vector3.forward), SendMessageOptions.DontRequireReceiver);
+                }
+                timer = fireGap;
+                flare.SetActive(true);
+                recoil += damage / 10f;
+                transform.Rotate(damage / -10f, 0, 0);
+                gun.RotateAround(Vector3.zero, Vector3.right, damage / -10f);
+            }
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0.07) { flare.SetActive(false); }
+        }
+        if (recoil > 0)
+        {
+            recoil -= Time.deltaTime * 20;
+            transform.Rotate(Time.deltaTime * 20, 0, 0);
+            gun.RotateAround(Vector3.zero, Vector3.right, Time.deltaTime * 20);
+        }
+    }
+}
---
+++ Pong/Assets/Assets (Editor)/Game Scripts/Gameplay Scripts/ShotAtScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotAtScript : MonoBehaviour
+{
+    public int health;
+    // Use this for initialization
+    void Start() { }
+
+    void shotAt(Vector3 damageVector)
+    {
+        GetComponent<Rigidbody>().AddForce(damageVector / 10, ForceMode.Impulse);
+        health -= (int)damageVector.magnitude + 1;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (health < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
