400fa91335d063fd3b442da5fd35b16b23629a79
alicefaere <alicefaere@users.noreply.github.com>
2018-02-19 19:35:22 -0500

    reenabled shooting cubes to death

--- Pong/Assets/Assets (Editor)/Game Scripts/Gameplay Scripts/ShotAtScript.cs
+++ Pong/Assets/Assets (Editor)/Game Scripts/Gameplay Scripts/ShotAtScript.cs
@@ -2,12 +2,20 @@
 
 public class ShotAtScript : MonoBehaviour
 {
+    private int curHealth;
     public int health;
+
+    void Start()
+    {
+        curHealth = health;
+    }
+
     public void ShotAt(Vector3 damageVector)
     {
         GetComponent<Rigidbody>().AddForce(damageVector / 10, ForceMode.Impulse);
-        health -= (int)damageVector.magnitude + 1;
-        if (health < 0)
+        curHealth -= (int)damageVector.magnitude + 1;
+        GetComponent<MeshRenderer>().material.color = new Color(1, (float)curHealth/health, (float)curHealth/health);
+        if (curHealth < 0)
         {
             Destroy(gameObject);
         }
