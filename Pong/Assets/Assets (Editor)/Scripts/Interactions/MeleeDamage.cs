7363a23d4411a5852f9c0371a9289efea78b6617
jap180 <jap180@case.edu>
2018-03-07 16:47:53 -0500

    function over form baby

---
+++ Pong/Assets/Assets (Editor)/Scripts/Interactions/MeleeDamage.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeDamage : MonoBehaviour {
+
+    public int health;
+    public int InvulnerabilityTime = 20;
+    private int counter;
+
+	// Use this for initialization
+	void Start () {
+        health = 3;
+        counter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (counter > 0) counter--;
+	}
+
+    public void TakeDamage()
+    {
+        health--;
+        if (health < 1) gameObject.SetActive(false);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<MeleeHitbox>() != null)
+        {
+
+            if (counter < 1)
+            {
+                counter = InvulnerabilityTime;
+                TakeDamage();
+            }
+        }
+    }
+}
---
+++ Pong/Assets/Assets (Editor)/Scripts/Interactions/MeleeHitbox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeHitbox : MonoBehaviour {
+
+    // I can't remember why else I needed this class besides GetComponent? 
+    // probably for directional knockback, but that's for later
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
