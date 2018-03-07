07d2fcb9209cf2578bd25fe5aab082a534390a99
Chance Lytle <chance_lytle@yahoo.com>
2018-03-07 17:59:47 -0500

    Keypad In Place
    Put the keypad in place and made other things inspectables

--- Pong/Assets/Assets (Editor)/Scripts/Inventory/InspectableInformation.cs
+++ Pong/Assets/Assets (Editor)/Scripts/Inventory/InspectableInformation.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class InspectableInformation : MonoBehaviour {
@@ -6,15 +7,17 @@ public class InspectableInformation : MonoBehaviour {
     public enum Type { Keypad, Note, None = -1 }
     public Type type = Type.None;
     public GameObject UIEquivalent; // messy coupling but rushing to push rn
-
-    public string id = "";
+    public GameObject corrDoor;
 
     public GameObject Inspectable;
 
-    // This is keypad exclusive code for now, won't get called unless type is keypad but this can be abstracted later
+    public string id = "";
+
+    // This is keypad exclusive code for now, won't get called unless type is keypad but will can be abstracted later
 
     private int curNum;
-    private int counter, curCode, correctCode = 6567; // hardcoded, in the end every keypad will have a door attached to it with the right code.
+    private bool unlocked;
+    private int counter, curCode, correctCode = 6757; // hardcoded, in the end every keypad will have a door attached to it with the right code.
     public void horMove(int move)
     {
         //Debug.Log("moved " + move);
@@ -34,22 +37,33 @@ public class InspectableInformation : MonoBehaviour {
 
     public void pressKey()
     {
-        Debug.Log("Pressed " + (curNum + 1));
-        counter++;
-        curCode = (curCode * 10) + curNum + 1;
-        updateText(curCode.ToString());
-        if (counter == 4) // 4 digit code, can be changed
+        if (!unlocked)
         {
-            if (curCode == correctCode) Debug.Log("You unlocked the door"); // and play right sound
-            else
+            Debug.Log("Pressed " + (curNum + 1));
+            counter++;
+            curCode = (curCode * 10) + curNum + 1;
+            updateText(curCode.ToString());
+            if (counter == 4) // 4 digit code, can be changed
             {
-                Debug.Log("Wrong code");
-                curCode = 0;
-                counter = 0;
-                updateText("");
-                // play bad sound here
+                if (curCode == correctCode) UnlockDoor(); // and play right sound
+                else
+                {
+                    Debug.Log("Wrong code");
+                    curCode = 0;
+                    counter = 0;
+                    updateText("");
+                    // play bad sound here
+                }
             }
         }
+    }
+
+    public void UnlockDoor()
+    {
+        Debug.Log("You unlocked the door");
+        unlocked = true;
+        // play good sound here
+        corrDoor.GetComponent<DoorToggle>().Toggle(true);
     }
 
     public void updateText(string input)
