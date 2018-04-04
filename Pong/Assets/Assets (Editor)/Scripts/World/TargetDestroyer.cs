61973dee63893932c9ca65a83ea4c11d1225db56
yuchen97ye <yuchen97ye@gmail.com>
2018-04-04 12:29:38 -0400

    Added (partially) target spawner

---
+++ Pong/Assets/Assets (Editor)/Scripts/World/TargetDestroyer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetDestroyer : MonoBehaviour {
+    private const float life = 1;
+    private float timer;
+    private Vector3 velocity;
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+        velocity = new Vector3(Random.Range(1, -1), Random.Range(1, -1), Random.Range(1, -1));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+        if (timer > life) { Destroy(gameObject); }
+	}
+}
---
+++ Pong/Assets/Assets (Editor)/Scripts/World/TargetSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSpawner : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject target;
+    private Vector3[] velocities = { Vector3.up, Vector3.down, Vector3.left, Vector3.right };
+    private const float freq = 2;
+    private float timer;
+	void Start () {
+        timer = freq;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer >= freq)
+        {
+            timer = 0;
+            Instantiate(target, new Vector3(Random.Range(1, -1), Random.Range(1, -1), 0), Quaternion.identity);
+        }
+	}
+}
