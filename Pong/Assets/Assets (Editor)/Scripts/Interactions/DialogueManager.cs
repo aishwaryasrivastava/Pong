3052e8cf28822357a5bb5fa90ba2eaf7f7d74294
jap180 <jap180@case.edu>
2018-04-18 21:18:46 -0400

    speak using controller
    just not visually apparent

--- Pong/Assets/Assets (Editor)/Scripts/Interactions/DialogueManager.cs
+++ Pong/Assets/Assets (Editor)/Scripts/Interactions/DialogueManager.cs
@@ -1,153 +1,213 @@
 ﻿using UnityEngine;
 using System;
+using System.Collections.Generic;
 using System.Xml;
 
-public class DialogueManager : MonoBehaviour {
+public class DialogueManager : MonoBehaviour
+{
 
-	public TextAsset XML;
-	private Dialogue dialog;
-	private Dialogue currentdialog;
+    public TextAsset XML;
+    private Dialogue dialog;
+    private Dialogue currentdialog;
+    public List<int> selections;
+    public int selIndex;
 
-	public bool talking;
-	private bool mouseover;
+    public bool talking;
+    private bool mouseover;
+    private bool madeSelection, changeSelection;
 
-	private string playerObject = "PlayerMan";
-	public PlayerMovementController control;
-	public PlayerInteractionController interact;
-	//public NPCScript owner;
-	private Inventory inventory;
 
-	//public GameObject itemToGive; // this will be ripped out and made a part of prisoners to whom a dialogue is attached, fine for now
+    private string playerObject = "PlayerMan";
+    public PlayerMovementController control;
+    public PlayerInteractionController interact;
+    //public NPCScript owner;
+    private Inventory inventory;
+    //public CanvasRenderer GuiPointer;
 
-	public int width = 20;
-	public int height = 20;
+    //public GameObject itemToGive; // this will be ripped out and made a part of prisoners to whom a dialogue is attached, fine for now
 
-	private GUIStyle custombutton, extraButton;
-	public int fontSize = 15;
+    public int width = 20;
+    public int height = 20;
 
-	public string name;
+    private GUIStyle custombutton, extraButton;
+    public int fontSize = 15;
 
-	private float displayTime = 0.0f;
-	private string display = "";
-	GameObject player;
+    public string name;
 
-	void Start () {
-		player = GameObject.Find (playerObject);
-		inventory = player.GetComponent<Inventory> ();
+    private float displayTime = 0.0f;
+    private string display = "";
+    GameObject player;
 
-		XmlDocument xmlDoc = new XmlDocument(); // xmlDoc is the new xml document.
-		xmlDoc.LoadXml(XML.text);
-		dialog = CreateTree (xmlDoc.FirstChild);
-		Cursor.lockState = CursorLockMode.Locked;
+    void Start()
+    {
+        player = GameObject.Find(playerObject);
+        inventory = player.GetComponent<Inventory>();
+        selections = new List<int>();
 
-		width = width * Screen.width / 100;
-		height = height * Screen.height / 100;
-	}
+        XmlDocument xmlDoc = new XmlDocument(); // xmlDoc is the new xml document.
+        xmlDoc.LoadXml(XML.text);
+        dialog = CreateTree(xmlDoc.FirstChild);
+        Cursor.lockState = CursorLockMode.Locked;
 
-	public void Reset()
-	{
-		XmlDocument xmlDoc = new XmlDocument(); // xmlDoc is the new xml document.
-		xmlDoc.LoadXml(XML.text);
-		dialog = CreateTree(xmlDoc.FirstChild);
-		Cursor.lockState = CursorLockMode.Locked;
+        width = width * Screen.width / 100;
+        height = height * Screen.height / 100;
+    }
 
-	}
+    public void Reset()
+    {
+        XmlDocument xmlDoc = new XmlDocument(); // xmlDoc is the new xml document.
+        xmlDoc.LoadXml(XML.text);
+        dialog = CreateTree(xmlDoc.FirstChild);
+        Cursor.lockState = CursorLockMode.Locked;
 
-	Dialogue CreateTree(XmlNode xml) {
-		Dialogue d = new Dialogue ();
-		XmlAttributeCollection attr = xml.Attributes;
-		d.Text = attr["text"].Value;
-		d.Option = attr["option"].Value;
-		d.Req = attr ["require"] != null ? attr ["require"].Value : "";
-		d.Take = attr["take"] != null ? attr["take"].Value : "";
-		d.Rep = attr ["rep"] != null ? int.Parse(attr ["rep"].Value) : 0;
-		d.RepMin = attr ["repMin"] != null ? int.Parse(attr ["repMin"].Value) : -1000;
-		d.RepMax = attr ["repMax"] != null ? int.Parse(attr ["repMax"].Value) : 1000;
-		if (attr ["sendX"] != null) {
-			d.sendToX = int.Parse (attr ["sendX"].Value);
-			d.sendToY = int.Parse (attr ["sendY"].Value);
-			d.sendToZ = int.Parse (attr ["sendZ"].Value);
-		} else {
-			d.sendToX = 0;
-			d.sendToY = 0;
-			d.sendToZ = 0;
-		}
+    }
 
-		if (attr ["item"] != null)
-		{
-			//Should be int to line up with ItemAttributeInformation options. This is for inventory icons mostly
-			//this is the name of the item basically (or the identifier for key-door connection)             
-			d.GiveItem = new Pickup(attr["id"].Value, (ItemAttributeInformation.Type)int.Parse(attr["item"].Value), interact.GetComponent<ItemIconHolder>());
-		}
-		else d.GiveItem = null;		
+    Dialogue CreateTree(XmlNode xml)
+    {
+        Dialogue d = new Dialogue();
+        XmlAttributeCollection attr = xml.Attributes;
+        d.Text = attr["text"].Value;
+        d.Option = attr["option"].Value;
+        d.Req = attr["require"] != null ? attr["require"].Value : "";
+        d.Take = attr["take"] != null ? attr["take"].Value : "";
+        d.Rep = attr["rep"] != null ? int.Parse(attr["rep"].Value) : 0;
+        d.RepMin = attr["repMin"] != null ? int.Parse(attr["repMin"].Value) : -1000;
+        d.RepMax = attr["repMax"] != null ? int.Parse(attr["repMax"].Value) : 1000;
+        if (attr["sendX"] != null)
+        {
+            d.sendToX = int.Parse(attr["sendX"].Value);
+            d.sendToY = int.Parse(attr["sendY"].Value);
+            d.sendToZ = int.Parse(attr["sendZ"].Value);
+        }
+        else
+        {
+            d.sendToX = 0;
+            d.sendToY = 0;
+            d.sendToZ = 0;
+        }
 
-		if (xml.HasChildNodes)
-		{
-			for (int i = 0; i < xml.ChildNodes.Count; i++)
-			{
-				d.AddChild (CreateTree (xml.ChildNodes [i]));
-			}
-		}
-		return d;
-	}
+        if (attr["item"] != null)
+        {
+            //Should be int to line up with ItemAttributeInformation options. This is for inventory icons mostly
+            //this is the name of the item basically (or the identifier for key-door connection)             
+            d.GiveItem = new Pickup(attr["id"].Value, (ItemAttributeInformation.Type)int.Parse(attr["item"].Value), interact.GetComponent<ItemIconHolder>());
+        }
+        else d.GiveItem = null;
 
-	// Update is called once per frame
-	void Update () {
+        if (xml.HasChildNodes)
+        {
+            for (int i = 0; i < xml.ChildNodes.Count; i++)
+            {
+                d.AddChild(CreateTree(xml.ChildNodes[i]));
+            }
+        }
+        return d;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetAxis("Cancel") > 0)
-		{
-			if (talking) EndDialogue ();
-		}
-		if (displayTime > 0) {
-			displayTime -= Time.deltaTime;
-		}
-	}
+        {
+            if (talking) EndDialogue();
+        }
+        if (talking)
+        {
+            if (Input.GetAxis("Submit") > 0)
+            {
+                if (!madeSelection)
+                {
+                    madeSelection = true;
+                    if (currentdialog.Children().Count > 0)
+                    {
+                        executeChoice(selections[selIndex]);
+                    }
+                    else
+                    {
+                        EndDialogue();
+                    }
+                }
 
-	public void LookingAt()
-	{
-		if (!talking)
-		{
-			mouseover = true;
-		}
-	}
+            }
+            else
+            {
+                madeSelection = false;
+            }
+            float tmp = Input.GetAxis("Vertical");
+            if (tmp != 0)
+            {
+                if (!changeSelection)
+                {
+                    changeSelection = true;
+                    changeSelectionIndex(tmp > 0);
+                }
 
-	public void NoLongerLookingAt()
-	{
-		mouseover = false;
-	}
+            }
+            else
+            {
+                changeSelection = false;
+            }
+        }
+        if (displayTime > 0)
+        {
+            displayTime -= Time.deltaTime;
+        }
+    }
 
-	void OnGUI() {
-		GUI.skin.box.wordWrap = true;
-		GUI.skin.button.wordWrap = true;
-		if (talking)
-		{
-			if (custombutton == null) {
-				custombutton = new GUIStyle ("button") { fontSize = fontSize };
-			}
-		    if (extraButton == null)
-		    {
-		        extraButton = new GUIStyle("button")
-		        {
-		            fontSize = fontSize,
-		            onHover = new GUIStyleState(),
-		            hover = new GUIStyleState()
-		        };
-		    }
-            GUI.Box (new Rect (20, 50, width, height), name+": "+currentdialog.Text, extraButton);
-			if (currentdialog.Children().Count == 0)
-			{
-				if (GUI.Button(new Rect(20, height + 60, width, height), "End", custombutton))
-				{
-					EndDialogue();
-				}
-			}
-			int count = 0;
-			for (int i = 0; i < currentdialog.Children().Count; i++)
-			{
-				Dialogue child = currentdialog.Children () [i];
-				if (InventoryCheck(child.Req) && ReputationCheck(child.RepMin,child.RepMax))
-				{
-					if (GUI.Button(new Rect(20, 60 + height * (count + 1), width, height), child.Option,custombutton))
-					{
+    public void LookingAt()
+    {
+        if (!talking)
+        {
+            mouseover = true;
+        }
+    }
+
+    public void NoLongerLookingAt()
+    {
+        mouseover = false;
+    }
+
+    void OnGUI()
+    {
+        GUI.skin.box.wordWrap = true;
+        GUI.skin.button.wordWrap = true;
+        if (talking)
+        {
+            selections = new List<int>();
+            //selIndex = 0;
+            if (custombutton == null)
+            {
+                custombutton = new GUIStyle("button") { fontSize = fontSize };
+            }
+            if (extraButton == null)
+            {
+                extraButton = new GUIStyle("button")
+                {
+                    fontSize = fontSize,
+                    onHover = new GUIStyleState(),
+                    hover = new GUIStyleState()
+                };
+            }
+            GUI.Box(new Rect(20, 50, width, height), name + ": " + currentdialog.Text, extraButton);
+            if (currentdialog.Children().Count == 0)
+            {
+                selections.Add(0);
+                if (GUI.Button(new Rect(20, height + 60, width, height), "End", custombutton))
+                {
+                    EndDialogue();
+                }
+            }
+            int count = 0;
+
+            for (int i = 0; i < currentdialog.Children().Count; i++)
+            {
+                Dialogue child = currentdialog.Children()[i];
+                if (InventoryCheck(child.Req) && ReputationCheck(child.RepMin, child.RepMax))
+                {
+                    selections.Add(i);
+                    if (GUI.Button(new Rect(20, 60 + height * (count + 1), width, height), child.Option, custombutton))
+                    {
+                        /**
 						var tmp = player.GetComponent<SoundController> ();
 						tmp.PlayScroll2 ();	
 						if (child.GiveItem != null)
@@ -172,53 +232,119 @@ public class DialogueManager : MonoBehaviour {
 							break;
 						}
 						ContinueDialogue(i);
-						break;
-					}
-					count++;
-				}
-			}
-		}
+                         */
 
-		if (displayTime > 0) {
-			GUI.Box (new Rect (Screen.width/2 - width/2, Screen.height/2 - height/2, width, height), display, extraButton);
-		}
-	}  
+                        executeChoice(i);
+                        break;
+                    }
+                    count++;
+                }
+            }
+        }
 
-	public void StartDialogue()
-	{
-		if (talking) return;
-		mouseover = false;
-		talking = true;
-		Cursor.lockState = CursorLockMode.None;
-		Cursor.visible = true;
-		currentdialog = dialog;     
-	}
+        if (displayTime > 0)
+        {
+            GUI.Box(new Rect(Screen.width / 2 - width / 2, Screen.height / 2 - height / 2, width, height), display, extraButton);
+        }
+    }
 
-	void ContinueDialogue(int i)
-	{
-		currentdialog = currentdialog.Children () [i];
-	}
+    private void executeChoice(int i)
+    {
+        Dialogue child = currentdialog.Children()[i];
+        var tmp = player.GetComponent<SoundController>();
+        tmp.PlayScroll2();
+        if (child.GiveItem != null)
+        {
+            tmp.PlayItem();
+            inventory.AddItem(child.GiveItem);
+            setDisplayText("Item added to inventory");
+            child.GiveItem = null;
+        }
+        if (child.Take.Length > 0)
+        {
+            inventory.RemoveThis(child.Take);
+        }
+        interact.Reputation[name] = interact.Reputation[name] + child.Rep;
+        if (child.Rep > 0)
+        {
+            tmp.PlayReputation();
+            setDisplayText("Reputation increased");
+        }
+        if (!(child.sendToX == 0 && child.sendToY == 0 && child.sendToZ == 0))
+        {
+            EndDialogue();
+            control.SetPosition(new Vector3(child.sendToX, child.sendToY, child.sendToZ));
 
-	void EndDialogue() {
-		var tmp = player.GetComponent<SoundController> ();
-		tmp.PlayEnd ();	
-		talking = false;
-		Cursor.lockState = CursorLockMode.Locked;
-		Cursor.visible = false;
-		control.LeaveConversation();
-	}
+        }
+        ContinueDialogue(i);
+    }
 
-	bool InventoryCheck(string id)
-	{
-		return id.Length == 0 || inventory.HaveItem(id);
-	}
+    public void StartDialogue()
+    {
+        if (talking) return;
+        mouseover = false;
+        talking = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        currentdialog = dialog;
+        //GuiPointer.gameObject.SetActive(true);
+    }
 
-	bool ReputationCheck(int r1, int r2) {
-		return interact.Reputation [name] > r1 && interact.Reputation[name] < r2;
-	}
+    void ContinueDialogue(int i)
+    {
+        selIndex = 0;
+        currentdialog = currentdialog.Children()[i];
+    }
 
-	void setDisplayText(string str) {
-		display = str;
-		displayTime = 1.5f;
-	}
+    void EndDialogue()
+    {
+        var tmp = player.GetComponent<SoundController>();
+        tmp.PlayEnd();
+        talking = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        control.LeaveConversation();
+        //GuiPointer.gameObject.SetActive(false);
+    }
+
+    bool InventoryCheck(string id)
+    {
+        return id.Length == 0 || inventory.HaveItem(id);
+    }
+
+    bool ReputationCheck(int r1, int r2)
+    {
+        return interact.Reputation[name] > r1 && interact.Reputation[name] < r2;
+    }
+
+    void setDisplayText(string str)
+    {
+        display = str;
+        displayTime = 1.5f;
+    }
+
+    void changeSelectionIndex(bool b)
+    {
+        if (selections.Count > 0)
+        {
+
+            if (b)
+            {
+                selIndex++;
+                if (selIndex == selections.Count)
+                {
+                    selIndex = 0;
+                }
+            }
+            else
+            {
+                selIndex--;
+                if (selIndex < 0)
+                {
+                    selIndex = selections.Count - 1;
+                }
+            }
+        }
+        //GuiPointer.GetComponent<RectTransform>().position.Set(20, 70 + height * (selIndex + 1), 0);
+    }
 }
